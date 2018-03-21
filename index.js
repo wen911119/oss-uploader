570835807e9be4306b3ee744092cc482675c0288
wen911119 <wen911119@gmail.com>
2018-03-21 10:37:18 +0800

    debug:env

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 console.log('oss-uploader-test')
 
+console.log('region:'+process.env.region)
+console.log('accessKeyId:'+process.env.accessKeyId)
+console.log('accessKeySecret:'+process.env.accessKeySecret)
+console.log('bucket:'+process.env.bucket)
+
+console.log('oss-uploader-test-end')
+
+
