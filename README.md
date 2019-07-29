cec52683d0f50a5576a95c1bbca22238fa74fc71
wen911119 <wen911119@gmail.com>
2019-07-29 15:21:07 +0800

    feat:升级

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# oss-uploader
+### 使用
+
+该镜像需要以下环境变量
+1. region（oss的region）
+2. accessKeyId（阿里云账户的accessKeyId,有权限访问oss的账户都可以）
+3. accessKeySecret（阿里云账户的accessKeySecret）
+4. bucket（阿里云oss的bucket名字）
+
+其它可选环境变量
+1. targetDir（oss的bucket里目录的名字，默认/代表根目录。如果要传到demo文件夹下，可以写/demo/）
--- index.js
+++ index.js
@@ -1,38 +1,34 @@
-const region = process.env.region
-let accessKeyId = process.env.accessKeyId
-let accessKeySecret = process.env.accessKeySecret
-const bucket = process.env.bucket
-const targetDir = process.env.targetDir || '/'
+const region = process.env.region;
+let accessKeyId = process.env.accessKeyId;
+let accessKeySecret = process.env.accessKeySecret;
+const bucket = process.env.bucket;
+const targetDir = process.env.targetDir || "/";
 
 if (region && accessKeyId && accessKeySecret && bucket) {
-    accessKeyId = accessKeyId.split('').reverse().join('')
-    accessKeySecret = accessKeySecret.split('').reverse().join('')
-    const fs = require('fs');
-    const OSS = require('ali-oss');
-    const path = require('path');
-    const co = require('co');
-    const client = new OSS({
-        region,
-        accessKeyId,
-        accessKeySecret,
-        bucket
-    });
-    const basePath = path.resolve('./bundle')
-    fs.readdir(basePath, function (err, files) {
-        files.forEach(function (file) {
-            console.log(basePath + '/' + file)
-            co(function* () {
-                var r1 = yield client.put(targetDir + file, basePath + '/' + file);
-                console.log('put success: %j', r1);
-            }).catch(function (err) {
-                console.error('error: %j', err);
-                throw new Error('upload oss fail !')
-            });
+  const fs = require("fs");
+  const OSS = require("ali-oss");
+  const path = require("path");
+  const client = new OSS({
+    region,
+    accessKeyId,
+    accessKeySecret,
+    bucket
+  });
+  const basePath = path.resolve("./bundle");
+  fs.readdir(basePath, function(err, files) {
+    files.forEach(function(file) {
+      console.log(basePath + "/" + file);
+      client
+        .put(targetDir + file, basePath + "/" + file)
+        .then(result => {
+          console.log("put success: %j", result);
         })
-    })
-
+        .catch(error => {
+          console.error("error: %j", error);
+          throw new Error("upload oss fail !");
+        });
+    });
+  });
 } else {
-    throw new Error('缺少必要环境变量！')
+  throw new Error("缺少必要环境变量！");
 }
-
-
